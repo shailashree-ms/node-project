b07d63901bc46183a5ce56a53ca143064ec3674c
CSHAIMS <shailashree.ms@bunge.com>
2025-02-27 17:31:42 +0530

    Add fs module with async/await to create a file

--- index.js
+++ index.js
@@ -11,3 +11,16 @@ const myPromise = new Promise((resolve, reject) => {
     .catch((error) => {
       console.error(error);
     });
+
+    const fs = require('fs').promises;
+
+    const writeToFile = async () => {
+      try {
+        await fs.writeFile('message.txt', 'Hello, this file was created using fs and async/await!');
+        console.log('File has been written!');
+      } catch (error) {
+        console.error('Error writing file:', error);
+      }
+    };
+    
+    writeToFile();
